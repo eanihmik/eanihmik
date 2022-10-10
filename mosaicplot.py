8313d551f8ffeef17ef9d178e2172b6aeb5cca13
Naehi Kim <57555570+eanihmik@users.noreply.github.com>
2022-10-10 22:41:14 +0900

    Update and rename mosaicplot to mosaicplot.py

--- mosaicplot.py
+++ mosaicplot.py
@@ -3,7 +3,7 @@ fig, ax = plt.subplots(figsize=(a, b))
 props = {}
 col_dic = {colname1: color1, colname2: color2, colname3: color3}
 for x, col in col_dic.items():
-  for y in rowarray:
-    props[(x, y)] = {"color": col}
+    for y in rowarray:
+        props[(x, y)] = {"color": col}
 mosaic(data=data, index=data.index, properties=props, ax=ax)
 plt.show()
